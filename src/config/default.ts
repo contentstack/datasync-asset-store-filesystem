608c59661bef04dc0922aac3a616342bbf74ef38
asmit-patil <patilasmit0@gmail.com>
2019-01-03 18:58:37 +0530

    initial commit

---
+++ src/config/default.ts
@@ -0,0 +1,48 @@
+
+/*!
+* contentstack-content-store-mongodb
+* copyright (c) Contentstack LLC
+* MIT Licensed
+*/ 
+
+export const defaultConfig =  {
+	"locales": [
+	  {
+		"code": "en-us",
+		"relative_url_prefix": "/"
+	  },
+	  {
+		"code": "es-es",
+		"relative_url_prefix": "/es/"
+	  },
+	  {
+		"code": "fr-fr",
+		"relative_url_prefix": "/fr/"
+	  }
+	],
+
+		"asset-connector": {
+    	"type": 'filesystem',
+      "pattern": '/assets/:uid/:filename',
+      "base_dir": './_contents',
+      "options": {
+        "dotfiles": 'ignore',
+        "etag": true,
+        "extensions": [ 'html', 'htm'],
+        "fallthrough": true,
+        "index": 'index.html',
+        "lastModified": true,
+        "maxAge": 0,
+        "redirect": true,
+        "setHeaders": (res, path, stat) => {
+          res.set('x-timestamp', Date.now());
+        }
+      }
+    }
+	//}
+	,
+	"contentstack": {
+	  "api_key": '***REMOVED***',
+	  "access_token": '***REMOVED***'
+	}
+  }
