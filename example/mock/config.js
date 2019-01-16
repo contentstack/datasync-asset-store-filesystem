cee2e55f26576208bd0d12f7f2407f8bd3661230
asmit-patil <patilasmit0@gmail.com>
2019-01-16 18:45:14 +0530

    updated example

---
+++ example/mock/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  contentstack: {
+    apiKey: 'bltd1343376dfba54d2',
+    token: 'cs12363bf3654a9b1dbf15a590'
+  },
+  'content-connector': {
+
+  },
+  'asset-connector': {
+
+  },
+  listener: {
+
+  },
+  plugins: {
+    myplugin: {
+      name: 'Kashi'
+    }
+  },
+  'sync-manager': {
+
+  }
+}
