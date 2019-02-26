af085a56d25e0fbe2f0d4f65a4db27aec7a4cdd6
asmit-patil <patilasmit0gmail.com>
2019-02-26 16:16:37 +0530

    updated config and .npmignore file

--- example/mock/config.js
+++ example/mock/config.js
@@ -1,13 +1,13 @@
 module.exports = {
   contentstack: {
-    apiKey: 'bltd1343376dfba54d2',
-    token: 'cs12363bf3654a9b1dbf15a590'
+    apiKey: '',
+    token: ''
   },
   'contentStore': {
 
   },
   'assetStore': {
-
+    baseDir: '../../../../auth/_contents'
   },
   listener: {
 
