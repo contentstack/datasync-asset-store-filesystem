76688a297baec4bb3bb2b419f5a17006e4688d29
iyerrama29 <iyerrama29+contentstack.github@gmail.com>
2019-06-02 23:07:30 +0530

    get/set config + getAssetStoreInstance methods added

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,6 @@
 const requiredKeysForPublish = ['url', 'locale', 'uid']
 const requiredKeysForUnpublish = ['url', 'locale', 'filename', 'uid', '_internal_url']
 
-
 export const validatePublishAsset = (asset) => {
     requiredKeysForPublish.forEach((key) => {
       if (!(key in asset)) {
@@ -17,50 +16,3 @@ export const validateUnPublishAsset = (asset) => {
       }
     })
 }
-
-export function extractDetails(type, asset, config) {
-    const values: any = []
-    let keys: string[]
-
-    if (type === 'internal') {
-      keys = config.internalUrlKeys
-    } else {
-      keys = config.folderPathKeys
-    }
-
-    if (config.assetFolderPrefixKey && typeof config.assetFolderPrefixKey === 'string') {
-      values.push(config.assetFolderPrefixKey)
-    }
-
-    const regexp = new RegExp('https://(assets|images).contentstack.io/(v[\\d])/assets/(.*?)/(.*?)/(.*?)/(.*)', 'g')
-    let matches
-
-    while ((matches = regexp.exec(asset.url)) !== null) {
-      if (matches && matches.length) {
-        if (matches[2]) {
-          asset.apiVersion = matches[2]
-        }
-        if (matches[3]) {
-          asset.apiKey = matches[3]
-        }
-        if (matches[4]) {
-          asset.downloadId = matches[4]
-        }
-      }
-    }
-
-    for (let i = 0, keyLength = keys.length; i < keyLength; i++) {
-      if (keys[i].charAt(0) === ':') {
-        const k = keys[i].substring(1)
-        if (asset[k]) {
-          values.push(asset[k])
-        } else {
-          throw new TypeError(`The key ${keys[i]} did not exist on ${JSON.stringify(asset)}`)
-        }
-      } else {
-        values.push(keys[i])
-      }
-    }
-
-    return values
-  }
